25ff250eaf8b600d3254787cf07678cfbd22d9c4
Eduard Sysoev <syspev.edik@gmail.com>
2024-03-12 11:40:33 +0300

    chore: 🔧 some changes

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -2,10 +2,11 @@ import { env } from '@/env.mjs';
 import type { SiteConfig } from '@/types/types';
 
 export const siteConfig: SiteConfig = {
-  name: 'Ноктюрн',
-  description: 'Ноктюрн - современный музыкальный сервис, который дарит вам доступ к бесконечному музыкальному миру.',
+  name: 'Бронница блог',
+  description:
+    'Самые свежие новости и события села - у нас на портале! Будь в курсе всех событий в нашем уютном поселении. Информация о жизни сельского района, культурных мероприятиях и интересных событиях. Подписывайся и не упускай важные новости!',
   url: env.NEXT_PUBLIC_APP_URL,
-  ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.png`,
+  ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
   links: { vkProfile: 'https://vk.com/sysoeev' },
   creator: 'Эдуард Сысоев'
 };
--- frontend/lib/fonts.ts
+++ frontend/lib/fonts.ts
@@ -3,11 +3,11 @@ import { Inter, Roboto_Mono } from 'next/font/google';
 export const fontSans = Inter({
   variable: '--font-sans',
   subsets: ['cyrillic', 'latin'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700'],
   fallback: ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
 });
 
-export const fontHeading = Roboto_Mono({
+export const fontMono = Roboto_Mono({
   subsets: ['cyrillic', 'latin'],
-  variable: '--font-heading'
+  variable: '--font-mono'
 });
